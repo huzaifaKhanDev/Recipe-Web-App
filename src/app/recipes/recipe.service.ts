c5718907a6d978de30f21ad77dfe7b499fe4acae
Huzaifa Khan <huzaifa.khan@hw.com.local>
2022-06-22 23:48:25 +0500

    recipe-components-communicaation-via-service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,8 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     {
       name: 'A Test Recipe',
@@ -9,16 +11,6 @@ export class RecipeService {
     },
     {
       name: 'Another Test Recipe',
-      description: 'This is a test recipe',
-      imagePath: 'https://www.simplyrecipes.com/thmb/mbN8mXZ0srgAT1YrDU61183t0uM=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1b-ea13798d224048b3a28afb0936c9b645.jpg'
-    },
-    {
-      name: 'A Test Recipe',
-      description: 'This is a test recipe',
-      imagePath: 'https://www.simplyrecipes.com/thmb/mbN8mXZ0srgAT1YrDU61183t0uM=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1b-ea13798d224048b3a28afb0936c9b645.jpg'
-    },
-    {
-      name: 'A Test Recipe',
       description: 'This is a test recipe',
       imagePath: 'https://www.simplyrecipes.com/thmb/mbN8mXZ0srgAT1YrDU61183t0uM=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1b-ea13798d224048b3a28afb0936c9b645.jpg'
     },
