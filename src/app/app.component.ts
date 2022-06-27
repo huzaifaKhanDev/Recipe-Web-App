433486a6ccc51cb0aeb591dfa234756bf5df6fb4
Huzaifa Khan <huzaifa.khan@hw.com.local>
2022-06-27 12:05:21 +0500

    ingredients-to-shopping-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,61 @@ export class AppComponent {
   onNavigate(feature: string){
     this.loadedFeature = feature;
   }
+  // selectedTheme: string = 'light';
+  constructor(){
+
+  }
+
+
+  // changeTheme(themeName: string) {
+  //   this.selectedTheme = themeName;
+  //   // get html body element
+  //   const bodyElement = document.body;
+
+  //   if (bodyElement) {
+  //     switch(this.selectedTheme){
+
+  //       case 'dark': {
+
+  //         if(bodyElement.className == 'light'){
+  //           bodyElement.classList.remove('light');
+  //         }
+  //         else if(bodyElement.className == 'colorful'){
+  //           bodyElement.classList.remove('colorful');
+  //         }
+
+  //         bodyElement.classList.add(this.selectedTheme);
+  //         break;
+  //       }
+
+  //       case 'light': {
+
+  //         if(bodyElement.className == 'dark'){
+  //           bodyElement.classList.remove('dark');
+  //         }
+  //         else if(bodyElement.className == 'colorful'){
+  //           bodyElement.classList.remove('colorful');
+  //         }
+
+  //         bodyElement.classList.add(this.selectedTheme);
+  //         break;
+  //       }
+
+  //       case 'colorful': {
+
+  //         if(bodyElement.className == 'dark'){
+  //           bodyElement.classList.remove('dark');
+  //         }
+  //         else if(bodyElement.className == 'light'){
+  //           bodyElement.classList.remove('light');
+  //         }
+
+  //         bodyElement.classList.add(this.selectedTheme);
+  //         break;
+  //       }
+  //     }
+  //   }
+  // }
+
+
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,22 +1,56 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient";
+import { ShoppinglistService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     {
-      name: 'A Test Recipe',
-      description: 'This is a test recipe',
-      imagePath: 'https://www.simplyrecipes.com/thmb/mbN8mXZ0srgAT1YrDU61183t0uM=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1b-ea13798d224048b3a28afb0936c9b645.jpg'
+      name: 'Laal Chafoor Rosh',
+      description: 'Super-tender meat - just awesome!',
+      imagePath: '../../assets/images/laal chafoor.jpg',
+       ingredients : [
+        {
+          name: 'Lamb Meat',
+          amount: 1
+        },
+        {
+          name: 'Potatoes',
+          amount: 10
+        },
+      ]
+
+
+      // ingredients: { name: 'Lamb Meat', amount: 1}
     },
     {
-      name: 'Another Test Recipe',
-      description: 'This is a test recipe',
-      imagePath: 'https://www.simplyrecipes.com/thmb/mbN8mXZ0srgAT1YrDU61183t0uM=/648x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1b-ea13798d224048b3a28afb0936c9b645.jpg'
+      name: 'Chapli Kebab',
+      description: 'A complete and delicious meal',
+      imagePath: '../../assets/images/ck.jpg',
+      ingredients :[
+        {
+        name: 'Beef Meat',
+        amount: 1
+        },
+        {
+        name: 'Tomatoes and Onions',
+        amount: 2
+        },
+        {
+        name: 'Animal Fat',
+        amount: 1
+        }
+    ]
     },
   ];
+  constructor(private shoppinglistService: ShoppinglistService){}
 
   getRecipes(){
     return this.recipes.slice();
+  }
+  addIngredientsToShoppinglist(ingredients: Ingredient[]){
+    this.shoppinglistService.addIngredients(ingredients)
   }
 }
